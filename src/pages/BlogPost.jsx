b9f8e5d9011bf7aa99e28654d0912b19b783933e
Prabhat Chandela <chandela.prabhat@gmail.com>
2024-10-05 17:14:39 +0530

    blog page layout updated

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import databaseService from "../appwrite/database_service";
 import bucketService from "../appwrite/bucket_service";
-import { Container } from "../components/index";
+import { Button} from "../components/index";
 import parse from "html-react-parser";
 import { useSelector } from "react-redux";
-import { VscEdit , VscClose } from "react-icons/vsc";
+import { VscEdit, VscClose } from "react-icons/vsc";
+import { FaHeading } from "react-icons/fa";
 
 export default function BlogPost() {
     const [post, setPost] = useState(null);
@@ -35,42 +36,46 @@ export default function BlogPost() {
     };
 
     return post ? (
-        <div className="py-8 ">
+        <div className="p-3 lg:p-7 grid place-items-center">
 
-            <Container>
-                <div className=" w-full flex flex-col items-center justify-center gap-5">
+            <main className="py-5 sm:py-10">
 
-                    <div className="w-full  flex justify-center items-center mb-4 relative border rounded-xl py-7 bg-orange-400">
-                        <img
-                            src={bucketService.getFilePreview(post.featuredimage)}
-                            alt={post.title}
-                            className="rounded-xl w-[60%] "
-                        />
+                <div className="max-w-5xl flex flex-col">
 
+                    <div className="w-full bg-white p-2 sm:p-5 flex flex-col gap-5">
+                        <h1 className="flex items-center gap-2 text-2xl text-black uppercase font-bold"><span className="bg-black text-white p-2 rounded-lg"><FaHeading /></span> {post.title}</h1>
                         {isAuthor && (
-                        
-                            <div className="">
+
+                            <div className="w-full flex gap-5">
                                 <Link to={`/edit-post/${post.$id}`}>
-                                    <button className="mr-3 absolute right-1 sm:right-6 top-6 bg-black text-sm text-orange-300 rounded-md font-semibold w-fit px-2 py-1">
-                                    <VscEdit />
-                                    </button>
+                                    <Button className="flex items-center justify-center gap-2">
+                                        <VscEdit /> <span>Edit Blog</span>
+                                    </Button>
                                 </Link>
-                               
-                                <button className="bg-black absolute left-2 sm:left-6 top-6 text-orange-300 text-sm rounded-md font-semibold w-fit px-2 py-1" onClick={deletePost}>
-                                <VscClose />
-                                </button>
+
+                                <Button className="flex items-center justify-center gap-2" onClick={deletePost}>
+                                    <VscClose /> <span>Delete Blog</span>
+                                </Button>
                             </div>
                         )}
                     </div>
-                    <div className="w-full px-3 mb-6">
-                        <h1 className="text-2xl text-center uppercase underline underline-offset-8 font-bold">{post.title}</h1>
+
+                    <div className="w-full overflow-hidden">
+                        <img
+                            src={bucketService.getFilePreview(post.featuredimage)}
+                            alt={post.title}
+                            className="w-full max-h-full object-center"
+                        />
                     </div>
-                    <div className="browser-css px-3 w-full">
+
+                    <div className="mt-5 w-full">
                         {parse(post.content)}
                     </div>
 
                 </div>
-            </Container>
+
+            </main>
+
         </div>
 
     ) : null;
